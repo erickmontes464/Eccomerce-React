b1d4eb273524cb567fd3e95f1425e3e2e4f070f9
erickmontes464 <erickmontes464@gmail.com>
2023-03-22 02:30:14 -0500

    agregue boton con vinculo

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/header.css';
 import {NavLink, Link} from 'react-router-dom'
 import Logo from '../images/logo.PNG';
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineHeart } from "react-icons/ai";
 import { RiAccountCircleFill } from "react-icons/ri";
 import { CgShoppingCart } from "react-icons/cg";
-import { AiOutlineHeart } from "react-icons/ai";
+import { FaArrowCircleUp } from "react-icons/fa";
+
 
 const Header = () => {
+  
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', mostrarBoton);
+    return () => {
+      window.removeEventListener('scroll', mostrarBoton);
+    };
+  }, []);
+
+  const mostrarBoton = () => {
+    /* Si el usuario ha desplazado hacia abajo más de 100 píxeles, mostramos el botón */
+    if (window.pageYOffset > 100) { 
+      setVisible(true);
+    } else { /* De lo contrario, lo ocultamos */
+      setVisible(false);
+    }
+  };
+
+/* Desplazamos suavemente hacia arriba cuando se hace clic en el botón */
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+  };
+
   return (
     <>
     <header className='header-top-strip'>
 
     </header>
-      <div className='d-flex justify-content-around'>
+      <div className='d-flex justify-content-around' id='vinculo'>
         <img className='logo' src={Logo} alt='Logo'/>   
         <div className='head-right'>
           <a href='/'>LOGIN</a>
@@ -32,6 +57,7 @@ const Header = () => {
         <NavLink to={'/'}><CgShoppingCart /></NavLink>
         <NavLink to={'/'}><AiOutlineHeart /></NavLink>
       </div>
+      {visible && <button className='btn-hight btn btn-dark pt-0' onClick={handleClick}><FaArrowCircleUp /></button>}
     </>
   )
 }
